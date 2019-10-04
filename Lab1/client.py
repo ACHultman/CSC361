fe7eab1534da9208a8b1cf1dd951af047cfd59b5
ACHultman <38937780+ACHultman@users.noreply.github.com>
2019-10-04 00:21:30 -0700

    First commit. Needs testing.

---
+++ Lab1/client.py
@@ -0,0 +1,22 @@
+from socket import *
+import sys
+
+try:
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+except error as msg:
+    print("Failed to create socket. Error code: " + str(msg[0]) + " , Error message : " + msg[1])
+    sys.exit()
+print("Socket Created")
+
+host_ip = sys.argv[1]
+host_port = sys.argv[2]
+file = sys.argv[3]
+
+clientSocket.connect((host_ip, host_port))
+print("Socket Connected to " + host_ip + " on ip " + host_ip)
+
+message = "GET /" + file + " HTTP/1.1\r\n\r\n"
+clientSocket.sendall(message)
+print("Message sent successfully")
+reply = clientSocket.recv(4096)
+print(reply)
---
+++ Lab1/server.py
@@ -0,0 +1,51 @@
+from socket import *
+import sys  # In order to terminate the program
+
+HOST = "127.0.0.4"
+PORT = 1234
+print("Server host: " + HOST)
+# Preparing server socket:
+
+try:
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+except error as msg:
+    print("Failed to create socket. Error code: " + str(msg[0]) + " , Error message: " + msg[1])
+    sys.exit()
+print("Server TCP Socket Created")
+
+try:
+    serverSocket.bind((HOST, PORT))
+except error as msg:
+    print("Bind failed. Error Message: " + str(msg))
+    sys.exit()
+print("Socket bind complete to port " + str(PORT))
+
+serverSocket.listen(10)
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    print("Connected with " + addr[0] + ":" + str(addr[1]))
+    try:
+        message = connectionSocket.recv(1024)
+        print(message.split()[1].decode("utf-8"))
+        filename = message.split()[1].decode("utf-8")
+        f = open(filename[1:])
+
+        outputdata = f.read()
+        # Send one HTTP header line into socket
+        response = b"HTTP/1.0 200 OK"
+        connectionSocket.sendall(response)
+
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+
+        connectionSocket.close()
+    except IOError:
+        # Send response message for file not found
+        connectionSocket.send(b"Error: File not found.")
+        # Close client socket
+        connectionSocket.close()
